1c1564ed2bca4ff9db4109188b3e7a409c58ec5d
Administrator <admin@example.com>
2021-08-26 12:58:10 +0200

    5. Create cargarCabecero, totalIngresos and totalIngresos

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
 const ingresos = [
     new Ingreso('Salario', 2100),
-    new Ingreso('Ventas', 300)
+    new Ingreso('Ventas', 1500)
 ];
 
 const egresos = [
-    new Egreso('Renta', 300),
-    new Egreso('Compras', 500)
+    new Egreso('Renta', 900),
+    new Egreso('Compras', 400)
 ];
 
 let cargarApp = () => {
     cargarCabecero();
 }
 
+let totalIngresos = () => {
+    let totalIngreso = 0;
+    for (let ingreso of ingresos) {
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = () => {
+    let totalEgreso = 0;
+    for (let egreso of egresos) {
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
 let cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos() / totalIngresos();
+    document.getElementById('presupuesto').innerHTML = presupuesto;
+    document.getElementById('porcentaje').innerHTML = porcentajeEgreso;
+    document.getElementById('ingresos').innerHTML = totalIngresos();
+    document.getElementById('egresos').innerHTML = totalEgresos();
 }
