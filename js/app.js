f60e85093aed65bca574824f4608df64aa81c59d
Administrator <admin@example.com>
2021-08-26 11:55:05 +0200

    4. Create Dato.js and app.js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const ingresos = [
+    new Ingreso('Salario', 2100),
+    new Ingreso('Ventas', 300)
+];
+
+const egresos = [
+    new Egreso('Renta', 300),
+    new Egreso('Compras', 500)
+];
+
+let cargarApp = () => {
+    cargarCabecero();
+}
+
+let cargarCabecero = () => {
+}
